5708ddaf4fa305f9dc63a7b929a5f02588e488cb
Matthew Hodgson <matthew@matrix.org>
2015-04-22 15:42:29 +0100

    remove bogus comment

--- examples/flux-chat/js/utils/ChatWebAPIUtils.js
+++ examples/flux-chat/js/utils/ChatWebAPIUtils.js
@@ -80,7 +80,6 @@ module.exports = {
   },
   
   createMessage: function(message) {
-    // simulate writing to a database
     var timestamp = Date.now();
     var id = 'm_' + timestamp;
     var threadID = message.threadID || ('t_' + Date.now());
